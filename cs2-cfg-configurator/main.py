d13fb8e58499b6c27a939f626a758674dd93e4bc
VitoS <ikn0@proton.me>
2026-06-20 21:48:31 +0200

    fix: update import path for MainWindow to reflect new app structure

--- cs2-cfg-configurator/main.py
+++ cs2-cfg-configurator/main.py
@@ -1,7 +1,7 @@
 # main.py
 import sys
 from PySide6.QtWidgets import QApplication
-from main_window import MainWindow
+from app.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
